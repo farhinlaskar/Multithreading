54e6d5396e8a47cd54728617d7a3622c981d6052
farhinlaskar <farhinmahmuda@gmail.com>
2023-03-08 12:47:13 +0530

    Commit done

---
+++ src/synchronisation/SyncDemo.java
@@ -0,0 +1,25 @@
+package synchronisation;
+
+public class SyncDemo {
+
+	public static void main(String[] args) {
+		Sender snd = new Sender(); 
+        ThreadedSend S1 = 
+            new ThreadedSend( " Hello " , snd ); 
+        ThreadedSend S2 = 
+            new ThreadedSend( " How are you? " , snd ); 
+        S1.start(); 
+        S2.start(); 
+        try
+        { 
+            S1.join(); 
+            S2.join(); 
+        } 
+        catch(Exception e) 
+        { 
+            System.out.println("Interrupted"); 
+        } 
+
+	}
+
+}
